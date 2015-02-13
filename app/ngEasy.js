6e900db2cd80a538aa6fddfbc03b0c630a612fb8
Yossi Halberstam <yossihalberstam@Yossis-Air.home>
2015-02-12 20:03:41 -0500

    added structure added jshint config files added adminNav directive

---
+++ app/ngEasy.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* We need to tell jshint what variables are being exported */
+/* global ngEasy: true
+ */
+
+////////////////////////////////////
+
+/**
+ * @ngdoc module
+ * @name ngEasy
+ * @module ngEasy
+ * @description
+ *
+ * # ngEasy (core module)
+ * The ngEasy module defines namespaces, and ties together all ngEasy library components.
+ * The module also contains any helper/common code to all components. The table below
+ * lists a high level breakdown of each of the services/factories, filters, directives and testing
+ * components available within this core module.
+ *
+ * <div doc-module-components="ng"></div>
+ */
+
+// Globals
+// var GLOBAL_VAR = 'value';    //comment
+
+
+var app = angular.module('ngEasy', []);
+
+app.inherits = function (Child, Parent) {
+	var Constructor = function () {};
+	Constructor.prototype = Parent.prototype;
+
+	Child.prototype = new Constructor();
+	Child.prototype.constructor = Child;
+};
