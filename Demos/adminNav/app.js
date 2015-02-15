c7ba01afa1b5342042404001b19621a9ee235ea1
YossiH <yossi@halberstam.info>
2015-02-14 23:38:12 -0500

    completed first draft of top nav with menus started sidenav with app area

--- Demos/adminNav/app.js
+++ Demos/adminNav/app.js
@@ -30,17 +30,19 @@ app.controller('ngEasyNavAdminDemoMainController', ['$scope', function($scope) {
 					state: 'demo'
 				},
 				topNavMenuGroups: {
-					position: 'left',
+					position: 'navbar-right',
 					menuGroup1: {
 						type: 'multiple',
 						isVisible: true,
 						iconClass: 'fa fa-info',
+						collapsedState: 'closed',
+						index: 0,
 						text: 'Menu Group 1',
 						menuItems: {
 							itemA: {
 								type: 'state',
 								state: 'demo',
-								iconClass: '',
+								iconClass: 'fa fa-info',
 								isVisible: true,
 								isDivider: false,
 								text: 'Item A',
@@ -51,7 +53,7 @@ app.controller('ngEasyNavAdminDemoMainController', ['$scope', function($scope) {
 								state: 'demo',
 								isVisible: true,
 								isDivider: false,
-								iconClass: '',
+								iconClass: 'fa fa-info',
 								text: 'Item B',
 								weight: 2
 							},
@@ -60,12 +62,50 @@ app.controller('ngEasyNavAdminDemoMainController', ['$scope', function($scope) {
 								state: 'demo',
 								isVisible: true,
 								isDivider: false,
-								iconClass: '',
+								iconClass: 'fa fa-info',
+								text: 'Item C',
+								weight: 1
+							}
+						}
+					},
+					menuGroup2: {
+						type: 'multiple',
+						isVisible: true,
+						iconClass: 'fa fa-info',
+						collapsedState: 'closed',
+						index: 1,
+						text: 'Menu Group 2',
+						menuItems: {
+							itemA: {
+								type: 'state',
+								state: 'demo',
+								iconClass: 'fa fa-info',
+								isVisible: true,
+								isDivider: false,
+								text: 'Item A',
+								weight: 0
+							},
+							itemB: {
+								type: 'state',
+								state: 'demo',
+								isVisible: true,
+								isDivider: false,
+								iconClass: 'fa fa-info',
+								text: 'Item B',
+								weight: 2
+							},
+							itemC: {
+								type: 'state',
+								state: 'demo',
+								isVisible: true,
+								isDivider: false,
+								iconClass: 'fa fa-info',
 								text: 'Item C',
 								weight: 1
 							}
 						}
 					}
+
 				}
 			},
 			sideBarSettings: {
--- app/directives/ngEasyAdminNav.js
+++ app/directives/ngEasyAdminNav.js
@@ -28,47 +28,46 @@ ngEasyModule
 						'</div>' +
 
 
-							'<ul ng-repeat="menuGroupItem in settings.topNavMenuGroups" class="nav navbar-top-links {{ settings.topNavMenuGroups.position }}">' +
-								'<li ng-switch="menuGroupItem.type" ng-if="menuGroupItem.isVisible" class="dropdown">' +
-									'<a ng-switch-when="state" ui-sref="{{ menuGroupItem.state }}" ui-sref-active="active">' +
-										'<span class="menu-item-icon fa fa-fw {{ menuGroupItem.iconClass}}"></span>' +
-										'<span class="menu-item-text"> {{ menuGroupItem.text }}</span>' +
-									'</a>' +
-									'<a ng-switch-when="action" ng-click="menuGroupItem.click()">' +
-										'<span class="menu-item-icon fa fa-fw {{ menuGroupItem.iconClass}}"></span>' +
-										'<span class="menu-item-text"> {{ menuGroupItem.text }}</span>' +
-									'</a>' +
-									'<a ng-switch-when="href" ng-href="menuGroupItem.href">' +
-										'<span class="menu-item-icon fa fa-fw {{ menuGroupItem.iconClass}}"></span>' +
-										'<span class="menu-item-text"> {{ menuGroupItem.text }}</span>' +
-									'</a>' +
-									'<a ng-switch-when="multiple" class="dropdown-toggle">' +
-										'<span class="fa fa-fw {{ menuGroupItem.iconClass }}"></span>' +
-										'<span> {{ menuGroupItem.text }} </span>' +
-										'<span class="fa fa-caret-down"></span>' +
-									'</a>' +
-									'<ul ng-switch-when="multiple" class="dropdown-menu">' +
-										'<li ng-repeat="menuItem in menuGroupItem.menuItems | orderBy: menuGroupItem.weight"' +
-											'ng-if="menuItem.isVisible">' +
-												'<span ng-switch="menuItem.type">' +
-													'<a ng-switch-when="state" ui-sref="{{ menuItem.state }}" ui-sref-active="active">' +
-														'<span class="menu-item-icon fa fa-fw {{ menuItem.iconClass}}"></span>' +
-														'<span class="menu-item-text"> {{ menuItem.text }}</span>' +
-													'</a>' +
-													'<a ng-switch-when="action" ng-click="menuItem.click()">' +
-														'<span class="menu-item-icon fa fa-fw {{ menuItem.iconClass}}"></span>' +
-														'<span class="menu-item-text"> {{ menuItem.text }}</span>' +
-													'</a>' +
-													'<a ng-switch-when="href" ng-href="menuGroupItemChild.href">' +
-														'<span class="menu-item-icon fa fa-fw {{ menuItem.iconClass}}"></span>' +
-														'<span class="menu-item-text"> {{ menuItem.text }}</span>' +
-													'</a>' +
-												'</span>' +
-										'</li>' +
-									'</ul>' +
-								'</li>' +
-							'</ul>' +
-					'</nav>',
+						'<ul ng-repeat="menuGroupItem in settings.topNavMenuGroups" class="nav navbar-top-links {{ settings.topNavMenuGroups.position }}">' +
+							'<li ng-switch="menuGroupItem.type" ng-if="menuGroupItem.isVisible" ng-click="toggleTopNavMenu(menuGroupItem.index);" class="dropdown {{menuGroupItem.collapsedState}}">' +
+								'<a ng-switch-when="state" ui-sref="{{ menuGroupItem.state }}" ui-sref-active="active">' +
+									'<span class="menu-item-icon fa fa-fw {{ menuGroupItem.iconClass}}"></span>' +
+									'<span class="menu-item-text"> {{ menuGroupItem.text }}</span>' +
+								'</a>' +
+								'<a ng-switch-when="action" ng-click="menuGroupItem.click()">' +
+									'<span class="menu-item-icon fa fa-fw {{ menuGroupItem.iconClass}}"></span>' +
+									'<span class="menu-item-text"> {{ menuGroupItem.text }}</span>' +
+								'</a>' +
+								'<a ng-switch-when="href" ng-href="menuGroupItem.href">' +
+									'<span class="menu-item-icon fa fa-fw {{ menuGroupItem.iconClass}}"></span>' +
+									'<span class="menu-item-text"> {{ menuGroupItem.text }}</span>' +
+								'</a>' +
+								'<a ng-switch-when="multiple" class="dropdown-toggle">' +
+									'<span class="fa fa-fw {{ menuGroupItem.iconClass }}"></span>' +
+									'<span> {{ menuGroupItem.text }} </span>' +
+									'<span class="fa fa-caret-down"></span>' +
+								'</a>' +
+								'<ul ng-switch-when="multiple" class="dropdown-menu">' +
+									'<li ng-repeat="menuItem in menuGroupItem.menuItems | orderBy: menuGroupItem.weight"' +
+										'ng-if="menuItem.isVisible">' +
+											'<span ng-switch="menuItem.type">' +
+												'<a ng-switch-when="state" ui-sref="{{ menuItem.state }}" ui-sref-active="active">' +
+													'<span class="menu-item-icon fa fa-fw {{ menuItem.iconClass}}"></span>' +
+													'<span class="menu-item-text"> {{ menuItem.text }}</span>' +
+												'</a>' +
+												'<a ng-switch-when="action" ng-click="menuItem.click()">' +
+													'<span class="menu-item-icon fa fa-fw {{ menuItem.iconClass}}"></span>' +
+													'<span class="menu-item-text"> {{ menuItem.text }}</span>' +
+												'</a>' +
+												'<a ng-switch-when="href" ng-href="menuGroupItemChild.href">' +
+													'<span class="menu-item-icon fa fa-fw {{ menuItem.iconClass}}"></span>' +
+													'<span class="menu-item-text"> {{ menuItem.text }}</span>' +
+												'</a>' +
+											'</span>' +
+									'</li>' +
+								'</ul>' +
+							'</li>' +
+						'</ul>',
 		restrict: 'E',
 		require: '^ngEasyAdminNav',
 		scope: {
@@ -78,16 +77,100 @@ ngEasyModule
 			scope.settings = ngEasyAdminNavController.getTopNavSettings();
 			console.log(scope.settings);
 
+			scope.toggleTopNavMenu = function(idx) {
+				angular.forEach(scope.settings.topNavMenuGroups,  function(value, key) {
+					if(angular.isObject(value)) {
+						if (value.index === idx) {
+							value.collapsedState = value.collapsedState === 'open' ? 'closed' : 'open';
+						} else {
+							value.collapsedState = 'closed';
+						}
+					}
+				} );
+
+			}
+
 		}
 	};
 
 
 })
 
+.directive('ngEasySideNav', function ()  {
+
+		return {
+			template:   '<div collapse="isNavbarCollapsed">' +
+							'<div class="navbar-default sidebar" role="navigation">' +
+								'<div class="sidebar-nav navbar-collapse">' +
+									'<ul class="nav">' +
+										'<li class="sidebar-search" ng-show="!_sidebarTextCollapse.isCollapsed && _sidebarSearch.isVisible">' +
+											'<form ng-submit="_sidebarSearch.submit()">' +
+												'<div class="input-group">' +
+													'<input type="text" class="form-control search-input" placeholder="{{\'Search\'}}"' +
+													'ng-model="_sidebarSearch.model">' +
+													'<span class="input-group-btn">' +
+														'<button class="btn btn-default" type="submit">' +
+															'<i class="fa fa-search"></i>' +
+														'</button>' +
+													'</span>' +
+												'</div>' +
+											'</form>' +
+										'</li>' +
+										'<li ng-repeat="item in sidebarMenuItems | orderBy:\'weight\'" ng-if="item._isVisible()"></li>' +
+										'<li ng-click="toggleSidebarTextCollapse()" ng-if="_sidebarTextCollapse.isVisible">' +
+											'<a>' +
+												'<span class="fa fa-fw" ng-class="_sidebarTextCollapse.isCollapsed ? \'fa-arrow-right\' : \'fa-arrow-left\'"></span>' +
+											'</a>' +
+										'</li>' +
+									'</ul>' +
+								'</div>' +
+							'</div>' +
+						'</div>' +
+					'</nav>',
+
+		restrict: 'E',
+			require: '^ngEasyAdminNav',
+			scope: {
+			},
+			link: function postLink(scope, element, attrs, ngEasyAdminNavController) {
+
+				scope.settings = ngEasyAdminNavController.getSideNavSettings();
+				console.log(scope.settings);
+
+			}
+		};
+
+
+	})
+
+
+	.directive('ngEasyAppArea', function ()  {
+
+		return {
+			template:   '<div id="app-area">' +
+						'<div class="row">' +
+						'<div class="col-lg-12">' +
+						'</div>' +
+						'</div>' +
+						'</div>',
+
+			restrict: 'E',
+			require: '^ngEasyAdminNav',
+			scope: {
+			},
+			link: function postLink(scope, element, attrs, ngEasyAdminNavController) {
 
 
 
-.directive('ngEasyAdminNav', ['$compile', function ($compile) {
+			}
+		};
+
+
+	})
+
+
+
+	.directive('ngEasyAdminNav', ['$compile', function ($compile) {
 
 	return {
 		template: '',
@@ -104,10 +187,17 @@ ngEasyModule
 		},
 		controller: ['$scope', function($scope) {
 			$scope.topNavSettings = $scope.settings.navSettings.topNavSettings;
+			$scope.sideBarSettings = $scope.settings.navSettings.sideBarSettings;
 
 			this.getTopNavSettings = function() {
 				return $scope.topNavSettings;
-			}
+			};
+
+			this.getSideNavSettings = function() {
+				return $scope.sideBarSettings;
+			};
+
+
 		}],
 
 
@@ -120,9 +210,12 @@ ngEasyModule
 			var topNavElement = angular.element('<ng-easy-top-nav></ng-easy-top-nav>');
 			element.append(topNavElement);
 			$compile(topNavElement)(scope);
-
-
-
+			var sideNavElement = angular.element('<ng-easy-side-nav></ng-easy-side-nav>');
+			element.append(sideNavElement);
+			$compile(sideNavElement)(scope);
+			var appArea = angular.element('<ng-easy-app-area></ng-easy-app-area>');
+			element.append(appArea);
+			$compile(appArea)(scope);
 
 
 		}
